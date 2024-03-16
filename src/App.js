6c8d482ffa5e09a254088f50757d85c2638f4778
Kelvin Ngan <nth1030@gmail.com>
2024-03-16 23:49:36 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useEffect} from 'react';
+
+// 83215b1c //
+
+const API_URL = 'http://www.omdbapi.com?apikey=83215b1c';
+
+const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+    
+    useEffect(()=>{
+        searchMovies('Spiderman');
+    }, []);
+    return (
+        <h1>App</h1>
+    );
+}
+export default App;
