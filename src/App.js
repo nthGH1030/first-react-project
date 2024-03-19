339d37ab7746d66126ee4d26ff8f8948cec26ad9
Kelvin Ngan <nth1030@gmail.com>
2024-03-20 00:42:58 +0800

    implement search function on seearch bar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const API_URL = 'http://www.omdbapi.com?apikey=83215b1c';
 
 const App = () => {
     const [ movies, setMovies ] = useState([]);
+    const [searchTerm, setSearchTerm] = useState();
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -26,17 +27,16 @@ const App = () => {
             <div className = "search">
                 <input
                 placeholder = "Search for movies"
-                value = "Superman"
-                onChange = {() => {}}
+                value = {searchTerm}
+                onChange = {(e) => {setSearchTerm(e.target.value)}}
                 />
                 <img    
                     src = {SearchIcon}
                     alt = "search"
-                    onClick = {() => {}}
+                    onClick = {() => {searchMovies(searchTerm)}}
                 />
             </div>
-            {
-                movies?.length > 0
+            {movies?.length > 0
                 ? (
                     <div className = "container">
                         {movies.map((movie)=> (
@@ -47,8 +47,7 @@ const App = () => {
                     <div className = "empty">
                         <h2>No movies found</h2>
                     </div>
-                )
-            }
+                )}
         </div>
     );
     
